7278e5c1a5a2df6f1fef8319f70ac84b1cd88498
c89387761-prog <c89387761@gmail.com>
2026-05-14 16:26:13 +0200

    Add Gradle configuration and project structure

---
+++ app/proguard-rules.pro
@@ -0,0 +1,4 @@
+# This is a configuration file for ProGuard.
+
+-keep public class * extends android.app.Service
+-keep public class * extends android.app.Activity
---
+++ app/src/main/java/com/godmode/worldmodel/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.godmode.worldmodel
+
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            val intent = Intent(this, FloatingService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+        }
+    }
+}
